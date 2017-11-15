a897f5f959bed2c6edab4b90dcfac8ea4ac37cfc
Minh Pham <phamhongminh@live.com>
2017-11-14 23:08:52 -0700

    Need another submit

--- Machine Learning/src/project/Utility.java
+++ Machine Learning/src/project/Utility.java
@@ -36,17 +36,16 @@ public class Utility {
 		return error;
 	}
 
-
 	protected static double[] update(double[] data, double[] weights, double l_rate) {
 		double y = data[0];
-		weights[0] = weights[0] + l_rate * y; // bias
+		weights[0] = y > 0 ? weights[0] + l_rate * y : weights[0] - l_rate; // bias
 		for (int i = 1; i < data.length; i++) {
-			weights[i] = weights[i] + l_rate * y * data[i];
+			weights[i] = y > 0 ? weights[i] + l_rate * y * data[i] : weights[i] + l_rate * (-1) * data[i];
 		}
 		return weights;
 	}
 
-	protected static int predictLabel (double[] data, double[] weights) {
+	protected static int predictLabel(double[] data, double[] weights) {
 		double sum = weights[0]; // bias
 		for (int i = 1; i < data.length; i++) {
 			sum += (double) weights[i] * data[i];
@@ -54,14 +53,14 @@ public class Utility {
 		int val = sum > 0 ? 1 : 0;
 		return val;
 	}
-	
+
 	protected static boolean predict(double[] data, double[] weights) {
 		double sum = weights[0]; // bias
 		for (int i = 1; i < data.length; i++) {
 			sum += (double) weights[i] * data[i];
 		}
-		int val = sum > 0 ? 1 : 0;
-		return val == data[0];
+		double val = sum > 0 ? 1 : 0;
+		return val != data[0];
 	}
 
 	protected static double[] initialWeight(int size) {
@@ -70,7 +69,7 @@ public class Utility {
 		double[] weight = new double[size];
 		for (int i = 0; i < size; i++) {
 			Random random = new Random();
-			weight[i] = min +  random.nextDouble()*(max - min);
+			weight[i] = min + random.nextDouble() * (max - min);
 		}
 		return weight;
 	}
@@ -82,7 +81,7 @@ public class Utility {
 			reader = new BufferedReader(new InputStreamReader(in));
 			String line;
 			while ((line = reader.readLine()) != null) {
-				dataset.add(Integer.parseInt(line));			
+				dataset.add(Integer.parseInt(line));
 			}
 
 		} catch (IOException e) {
@@ -96,6 +95,7 @@ public class Utility {
 		}
 		return dataset;
 	}
+
 	protected static ArrayList<double[]> parseString(InputStream in) {
 		ArrayList<double[]> dataset = new ArrayList<double[]>();
 		BufferedReader reader = null;
@@ -116,7 +116,7 @@ public class Utility {
 					value = Double.parseDouble(numb[1]);
 					data[index] = value;
 				}
-				dataset.add(data);			
+				dataset.add(data);
 			}
 
 		} catch (IOException e) {
@@ -130,24 +130,33 @@ public class Utility {
 		}
 		return dataset;
 	}
-	
-	protected static void writeToCSV(double[] trainWeight, ArrayList<Integer> inputID, ArrayList<double[]> input, String filename) throws FileNotFoundException {
-		BufferedWriter writer = null;
-		
+
+	protected static void writeToCSV(double[] trainWeight, ArrayList<Integer> inputID, ArrayList<double[]> input,
+			String filename) throws FileNotFoundException {
+		// BufferedWriter writer = null;
+		File file = new File(filename);
+		FileWriter writer = null;
 		try {
-			writer = new BufferedWriter(new FileWriter(filename));
-			writer.write("Id");
-			writer.write(",");
-			writer.write("Prediction");
-			writer.newLine();
-	        int inputSize = input.size();
-	        for (int i = 0; i < inputSize; i++) {
-	        		int label = predictLabel(input.get(i), trainWeight);
-	    			writer.write(inputID.get(i));
-	    			writer.write(",");
-	    			writer.write(label);
-	    			writer.newLine();
-	        }
+			// writer = new BufferedWriter(new FileWriter(filename));
+			writer = new FileWriter(filename, false);
+			writer.append("Id,Prediction");
+			writer.append("\n");
+			// writer.write("Id");
+			// writer.write(",");
+			// writer.write("Prediction");
+			// writer.newLine();
+			int inputSize = input.size();
+			for (int i = 0; i < inputSize; i++) {
+				int label = predictLabel(input.get(i), trainWeight);
+				writer.append(inputID.get(i).toString());
+				writer.append(",");
+				writer.append(String.valueOf(label));
+				writer.append("\n");
+				// writer.write(inputID.get(i));
+				// writer.write(",");
+				// writer.write(label);
+				// writer.newLine();
+			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		} finally {
